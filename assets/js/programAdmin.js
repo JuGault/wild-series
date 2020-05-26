ed716baf73de9e1c9f543a7a12e1d6fc4e4e21ca
Julien Gault <jugault45@gmail.com>
2020-05-26 19:19:41 +0200

    program_admin

---
+++ assets/js/programAdmin.js
@@ -0,0 +1,7 @@
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/programAdmin.scss';
+const $ = require('jquery');
+require('bootstrap');
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
